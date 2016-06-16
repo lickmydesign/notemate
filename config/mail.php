cd25853477fa8d46db8d5df002e45bf7d56a8621
Adam O'Dwyer <info@lickmydesign.co.uk>
2016-06-16 18:47:26 +0100

    add default auth scaffolding

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'support@notemate.com', 'name' => 'Note Mate Support'],
 
     /*
     |--------------------------------------------------------------------------
