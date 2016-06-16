17697d2198912176c5e2a4ed9559f12e933f91d6
Adam O'Dwyer <info@lickmydesign.co.uk>
2016-06-16 21:09:38 +0100

    fix if and wrapper around latest notes

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -8,44 +8,49 @@
 		<a href="/note" class="btn btn-success"><i class="fa fa-plus"></i> Add Note</a>
 	</p>
 
-	@if (count($latest_notes) > 0)
-		<div class="panel-body">
-			<table class="table table-striped">
-				<thead>
-				<tr>
-					<th>Title</th>
-					<th>Actions</th>
-				</tr>
-				</thead>
-				<tbody>
-				@foreach ($latest_notes as $note)
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			Your Latest Notes
+		</div>
+		@if (count($latest_notes) > 0)
+			<div class="panel-body">
+				<table class="table table-striped">
+					<thead>
 					<tr>
-						<!-- Note Name -->
-						<td class="table-text">
-							<a href="/notes/{{ $note->id }}" title="View Note">{{ $note->title }}</a>
-						</td>
-
-						<td>
-							<form action="{{ url('notes/'.$note->id) }}" method="POST">
-								{{ csrf_field() }}
-								{{ method_field('DELETE') }}
-
-								<a href="/notes/{{ $note->id }}/edit" class="btn btn-warning"><i class="fa fa-pencil"></i> Edit</a>
-
-								<button type="submit" class="btn btn-danger">
-									<i class="fa fa-trash"></i> Delete
-								</button>
-							</form>
-						</td>
+						<th>Title</th>
+						<th>Actions</th>
 					</tr>
-				@endforeach
-				</tbody>
-			</table>
-		</div>
-	@else
-		<div class="panel-body text-muted">
-			No notes available.
-		</div>
-	@endif
+					</thead>
+					<tbody>
+					@foreach ($latest_notes as $note)
+						<tr>
+							<!-- Note Name -->
+							<td class="table-text">
+								<a href="/notes/{{ $note->id }}" title="View Note">{{ $note->title }}</a>
+							</td>
+
+							<td>
+								<form action="{{ url('notes/'.$note->id) }}/delete" method="POST">
+									{{ csrf_field() }}
+									{{ method_field('DELETE') }}
+
+									<a href="/notes/{{ $note->id }}/edit" class="btn btn-warning"><i class="fa fa-pencil"></i> Edit</a>
+
+									<button type="submit" class="btn btn-danger">
+										<i class="fa fa-trash"></i> Delete
+									</button>
+								</form>
+							</td>
+						</tr>
+					@endforeach
+					</tbody>
+				</table>
+			</div>
+		@else
+			<div class="panel-body text-muted">
+				No notes available.
+			</div>
+		@endif
+	</div>
 
 @endsection
