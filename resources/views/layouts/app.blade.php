5a074cff2ec2a323b48ef1ff152dbc64cef26df6
Adam O'Dwyer <info@lickmydesign.co.uk>
2016-06-16 20:58:31 +0100

    add footer, logo and styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@ $app_name = Config::get('constants.app_name');
 	<!-- Styles -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="/css/site.css" />
 </head>
 <body id="app-layout">
 	<nav class="navbar navbar-default navbar-static-top">
@@ -36,7 +36,7 @@ $app_name = Config::get('constants.app_name');
 
 				<!-- Branding Image -->
 				<a class="navbar-brand" href="{{ url('/') }}">
-					Note Mate
+					<i class="fa fa-book"></i> Note Mate
 				</a>
 			</div>
 
@@ -74,6 +74,10 @@ $app_name = Config::get('constants.app_name');
 		@include('common.flash')
 		@yield('content')
 	</div>
+
+	<footer class="container">
+		<p class="text-muted">&copy; <?=date('Y'); ?> Acme Inc. All rights reserved.</p>
+	</footer>
 
 	<!-- JavaScripts -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
