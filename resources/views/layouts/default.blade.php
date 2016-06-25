338d26df0180c734b5016fcb53f8a3c68454547e
Adam O'Dwyer <info@lickmydesign.co.uk>
2016-06-25 10:54:58 +0100

    add icon to username

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -53,11 +53,11 @@
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-								{{ Auth::user()->name }} <span class="caret"></span>
+								<i class="fa fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
 							</a>
 
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+								<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
 							</ul>
 						</li>
 					@endif
